f0c0c50ff2496a3ed8b42a78a3bbce7a8aa84a48
rajmaravanthe <rraajjpoojary@gmail.com>
2021-06-12 12:43:58 +0530

    changed css

--- src/components/ToDoTable.js
+++ src/components/ToDoTable.js
@@ -9,12 +9,17 @@ const ToDoTable = ({ list, editItem, deleteItem }) => {
         {list &&
           list.map(({ key, value }) => (
             <ListGroup.Item key={key}>
-              {value}
-              <div onClick={() => editItem(key, value)} className='icon-list'>
-                <i className='fa fa-edit'></i>
-              </div>
-              <div onClick={() => deleteItem(key)} className='icon-list'>
-                <i className='fa fa-trash'></i>
+              <div className='content'>{value}</div>
+              <div className='icons'>
+                <span
+                  onClick={() => editItem(key, value)}
+                  className='icon-list'
+                >
+                  <i className='fa fa-edit'></i>
+                </span>
+                <span onClick={() => deleteItem(key)} className='icon-list'>
+                  <i className='fa fa-trash'></i>
+                </span>
               </div>
             </ListGroup.Item>
           ))}
